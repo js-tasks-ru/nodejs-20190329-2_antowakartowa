c8e43f47cd96ceeea2908a526bbeb57b3cc2b2dd
AntowaKartowa <gintonyk+gitkraken@googlemail.com>
2019-04-08 01:45:38 +0300

    2-module 1-task: add stream functionality throwing error when data limit is exceeded

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.counter = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    if ((this.counter += chunk.length) > this.limit) {
+      callback(new LimitExceededError());
+    }
 
+    this.push(chunk);
+    callback();
   }
 }
 
