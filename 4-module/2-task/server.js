f33ff7e8a727de8e596c45c7c8560db7f9f8fd1e
AntowaKartowa <gintonyk+gitkraken@googlemail.com>
2019-04-16 20:43:27 +0300

    4-module 2-task: Change to custom upload functionality implementation

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,37 +1,67 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
-const receiveFile = require('./receiveFile');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  const pathItems = url.parse(req.url).pathname.split('/');
+  const filepath = path.join(__dirname, 'files', pathItems.slice(-1)[0]);
 
-  if (pathname.includes('/') || pathname.includes('..')) {
-    res.statusCode = 400;
-    res.end('Nested paths are not allowed');
-    return;
-  }
+  let errorCode = req.method !== 'POST' && 501 ||
+    pathItems.length > 2 && 400 ||
+    req.headers['content-length'] > 1e6 && 413 ||
+    false;
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const messages = {
+    400: 'Invalid path',
+    409: 'File already exists',
+    413: 'File is too large',
+    500: 'Server error',
+    501: 'Not implemented',
+    201: 'File uploaded succesfully',
+  };
 
-  switch (req.method) {
-    case 'POST':
-      if (!filepath) {
-        res.statusCode = 404;
-        res.end('File not found');
-        return;
-      }
+  const responde = (code) => {
+    res.statusCode = code;
+    res.setHeader('Connection', 'close');
+    res.end(messages[code]);
+  };
 
-      receiveFile(filepath, req, res);
+  const deleteFile = (path) => {
+    fs.unlink(path, () => {});
+  };
 
-      break;
+  const handleLimitError = ({code}) => {
+    errorCode = code === 'LIMIT_EXCEEDED' ? 413 : 500;
+    responde(errorCode);
+    deleteFile(filepath);
+  };
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  const handleWriteError = ({code}) => {
+    errorCode = code === 'EEXIST' ? 409 : 500;
+    responde(errorCode);
+    if (errorCode === 500) deleteFile(filepath);
+  };
+
+  if (errorCode) {
+    responde(errorCode);
+  } else {
+    const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+    const limitStream = new LimitSizeStream({limit: 1048576});
+
+    req
+      .pipe(limitStream)
+      .on('error', handleLimitError)
+      .pipe(writeStream)
+      .on('error', handleWriteError)
+      .on('close', () => responde(201));
+
+    res.on('close', () => {
+      if (!res.finished) deleteFile(filepath);
+    });
   }
 });
 
