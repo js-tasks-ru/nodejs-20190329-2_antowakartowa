65da5374eaf1e31b659a2c7e990915c029637c19
Antowa Kartowa <gin.tonyk@gmail.com>
2019-04-17 23:41:14 +0300

    4-module 3-task: Add deleting file functionality

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,22 +1,43 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  const pathItems = url.parse(req.url).pathname.split('/');
+  const filepath = path.join(__dirname, 'files', pathItems.slice(-1)[0]);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  let errorCode = req.method !== 'DELETE' && 501 ||
+    pathItems.length > 2 && 400 ||
+    false;
 
-  switch (req.method) {
-    case 'DELETE':
+  const messages = {
+    400: 'Invalid path',
+    404: 'File doesn\'t exists',
+    500: 'Server error',
+    501: 'Not implemented',
+    200: 'File deleted',
+  };
 
-      break;
+  const responde = (code) => {
+    res.statusCode = code;
+    res.setHeader('Connection', 'close');
+    res.end(messages[code]);
+  };
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  if (errorCode) {
+    responde(errorCode);
+  } else {
+    fs.unlink(filepath, (error) => {
+      if (error) {
+        errorCode = error.code === 'ENOENT' ? 404 : 500;
+        responde(errorCode);
+      } else {
+        responde(200);
+      }
+    });
   }
 });
 
