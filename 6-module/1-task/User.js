e1c7aa4925a0b438e066d921d9b604e199368857
AntowaKartowa <gintonyk+gitkraken@googlemail.com>
2019-04-27 00:39:01 +0300

    6-module 1-task: Define user schema according to criteria

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: 'Email is required',
+    unique: 'This Email already exists',
+    match: '/^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/',
+    lowercase: true,
+    trim: true,
+  },
+  displayName: {
+    type: String,
+    required: 'Display name is required',
+    index: true,
+    trim: true,
+  },
+}, {timestamps: true});
 
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model('User', userSchema);
