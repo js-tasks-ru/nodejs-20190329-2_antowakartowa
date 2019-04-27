3e293daec7051e0290b0e3832e51201c2dee52d3
Antowa Kartowa <gin.tonyk@gmail.com>
2019-04-27 20:55:48 +0300

    6-module 2-task: Write users CRUD endpoints logic

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,5 +1,8 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const ObjectId = require('mongoose').Types.ObjectId;
+const User = require('./models/User');
+
 
 const app = new Koa();
 
@@ -12,34 +15,86 @@ app.use(async (ctx, next) => {
   } catch (err) {
     if (err.status) {
       ctx.status = err.status;
-      ctx.body = { error: err.message };
+      ctx.body = {error: err.message};
     } else {
       ctx.status = 500;
-      ctx.body = { error: 'Internal server error' };
+      ctx.body = {error: 'Internal server error'};
     }
   }
 });
 
+const handleError = (error, ctx) => {
+  const {errors} = error;
+  ctx.status = 400;
+  ctx.body = {
+    errors: errors
+      ? Object.keys(errors).reduce((errObj, key) => ({...errObj, [key]: errors[key].message}), {})
+      : error.message,
+  };
+};
+
 const router = new Router();
 
 router.get('/users', async (ctx) => {
-
+  const users = await User.find({});
+  ctx.body = users;
 });
 
 router.get('/users/:id', async (ctx) => {
+  try {
+    const user = await User.findOne({_id: new ObjectId(ctx.params.id)});
 
+    ctx.status = user ? 200 : 404;
+    ctx.body = user || 'User not found';
+  } catch (error) {
+    handleError(error, ctx);
+  }
 });
 
 router.patch('/users/:id', async (ctx) => {
+  const {email, displayName} = ctx.request.body;
 
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+        {_id: new ObjectId(ctx.params.id)},
+        {email, displayName},
+        {
+          omitUndefined: true,
+          runValidators: true,
+          new: true,
+        }
+    );
+
+    ctx.status = 200;
+    ctx.body = updatedUser;
+  } catch (error) {
+    handleError(error, ctx);
+  }
 });
 
 router.post('/users', async (ctx) => {
+  const {email, displayName} = ctx.request.body;
 
+  try {
+    const newUser = await User.create({email, displayName});
+
+    ctx.status = 200;
+    ctx.body = newUser;
+  } catch (error) {
+    handleError(error, ctx);
+  }
 });
 
 router.delete('/users/:id', async (ctx) => {
+  try {
+    const {deletedCount, ok} = await User.deleteOne({_id: new ObjectId(ctx.params.id)});
+    const isDeleted = !!(deletedCount && ok);
 
+    ctx.status = isDeleted ? 200 : 404;
+    ctx.body = isDeleted ? 'User deleted' : 'User not found';
+  } catch (error) {
+    handleError(error, ctx);
+  }
 });
 
 app.use(router.routes());
